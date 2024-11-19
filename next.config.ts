0ecf36f2fc09e17616fd789397c7558e58662058
rdev2021 <rdev2021@hotmail.com>
2024-11-19 14:49:32 +0530

    Adding hash in footer

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
+const { execSync } = require('child_process');
+const getGitHash = () => {
+  try {
+    return execSync('git rev-parse HEAD').toString().trim();
+  } catch (e) {
+    return 'no-git-hash';
+  }
+};
 
 const nextConfig: NextConfig = {
   output: 'export',
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_GIT_HASH: getGitHash(),
+  },
 };
 
 export default nextConfig;
